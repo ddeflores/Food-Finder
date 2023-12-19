0a7834b6fda0430b26e7ae69bf89c2602ca3b750
ddeflores <94253526+ddeflores@users.noreply.github.com>
2023-12-18 19:33:23 -0500

    add inital display and buttons

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native';
+import {camera, cameraType} from 'expo-camera';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+    if (!result.canceled) {
+      console.log(result);
+    }
+  };
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View styles={styles.imageContainer}>
+        <Image source={require('./placeholder.png')} style={styles.img} accessibilityLabel="placeholder image" />
+      </View>
+      <TouchableOpacity style={styles.button} onPress={pickImageAsync}>
+        <Text style={styles.text}>Upload Photo</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={pickImageAsync}>
+        <Text style={styles.text}>Take Photo</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +33,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#18191A',
     justifyContent: 'center',
+    alignItems: 'center',
   },
+  button: {
+    marginTop: 10,
+    backgroundColor: "#3A3B3C",
+    borderRadius: 18,
+    width: 320,
+    height: '6%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  img : {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  text: {
+    color: "white",
+    fontSize: 'auto',
+    fontSize: 16,
+    fontWeight: 'bold'
+  }
 });
